885e44241176f78f80776b7edbe1e5387ae9c461
Paxysocial <69212537+Paxysocial@users.noreply.github.com>
2020-08-27 22:56:29 +0200

    try2

--- main.js
+++ main.js
@@ -4,23 +4,35 @@ $('#div-chat').hide();
 
 let customConfig;
 
-$.ajax({
-  url: "https://global.xirsys.net",
-  data: {
-    ident: "paxy",
-    secret: "19ff98ce-d676-11ea-8660-0242ac150002",
-    domain: "Paxysocial.github.io",
-    application: "default",
-    room: "default",
-    secure: 1
-  },
-  success: function (data, status) {
-    // data.d is where the iceServers object lives
-    customConfig = data.d;
-    console.log(customConfig);
-  },
-  async: false
+
+
+// Node Get ICE STUN and TURN list
+let o = {
+    format: "urls"
+};
+
+let bodyString = JSON.stringify(o);
+let https = require("https");
+let options = {
+    host: "global.xirsys.net",
+    path: "/_turn/Paxysocial.github.io",
+    method: "PUT",
+    headers: {
+        "Authorization": "Basic " + Buffer.from("paxy:19ff98ce-d676-11ea-8660-0242ac150002").toString("base64"),
+        "Content-Type": "application/json",
+        "Content-Length": bodyString.length
+    }
+};
+let httpreq = https.request(options, function(httpres) {
+    let str = "";
+    httpres.on("data", function(data){ str += data; });
+    httpres.on("error", function(e){ console.log("error: ",e); });
+    httpres.on("end", function(){ 
+        console.log("ICE List: ", str);
+    });
 });
+httpreq.on("error", function(e){ console.log("request error: ",e); });
+httpreq.end();
 
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
     $('#div-chat').show();
